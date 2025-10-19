9738ba24a6026592d7088e6d6d35fc2fb784684a
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-19 22:33:31 +0530

    basic fumadocs setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { RootProvider } from "fumadocs-ui/provider/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import "@/app/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen antialiased`}
       >
-        {children}
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
---
+++ src/types/global.d.ts
@@ -0,0 +1,2 @@
+// Suppress biome error - Cannot find module or type declarations for side-effect import of '@/app/globals.css' in /app/layout.tsx
+declare module "*.css";
