ec9482e9a4dfc6603f2a7a45d215460aa9907a57
Abhishek Kumar <github.abhiarya@gmail.com>
2025-10-20 23:09:39 +0530

    basic setup for docs

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,22 @@
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import {
+  defineDocs,
+  defineConfig,
+  defineCollections,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
 
 export const docs = defineDocs({
   dir: "src/content/docs",
+});
+
+export const blogs = defineCollections({
+  type: "doc",
+  dir: "src/content/blogs",
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.iso.date().or(z.date()),
+  }),
 });
 
 export default defineConfig();
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,7 +1,12 @@
-import { docs } from "@/source";
+import { docs, blogs as blogPosts } from "@/source";
 import { loader } from "fumadocs-core/source";
+import { createMDXSource } from "fumadocs-mdx";
 
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+});
+
+export const blogs = loader(createMDXSource(blogPosts), {
+  baseUrl: "/blogs",
 });
